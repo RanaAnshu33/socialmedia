3c0f3c36f2de26d8dca06d0849a7dae5829dd4a0
RanaAnshu33 <ranaanshu7763@gmail.com>
2026-03-04 00:17:12 +0530

    upload image

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -24,8 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('userauth.urls'))
 ]
-
-urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
